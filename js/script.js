f8791c5ecdc635d72b6aa38557ee6be04214d4d8
R1verb <128699019+R1verb@users.noreply.github.com>
2024-04-08 14:48:01 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -183,3 +183,31 @@ copyTextElements.forEach(function(element) {
 });
 
 // COPYTEXT SETTINGS
+
+
+// R1VERB EXTENSIONS
+
+document.addEventListener("DOMContentLoaded", function() {
+  // R1VERB EXTENSIONS
+  var button = document.getElementById('r1verb_extensions');
+  if (button) {
+      button.addEventListener('click', function() {
+          var linksToDownload = [
+              "https://chromewebstore.google.com/detail/sponsorblock-for-youtube/mnjggcdmjocbbbhaepdhchncahnbgone?hl=ru&utm_source=ext_sidebar",
+              "https://chromewebstore.google.com/detail/adblock-%E2%80%94-%D0%BB%D1%83%D1%87%D1%88%D0%B8%D0%B9-%D0%B1%D0%BB%D0%BE%D0%BA%D0%B8%D1%80%D0%BE%D0%B2/gighmmpiobklfepjocnamgkkbiglidom?hl=ru&utm_source=ext_sidebar",
+              "https://chromewebstore.google.com/detail/shazam-%D0%B8%D1%89%D0%B8%D1%82%D0%B5-%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F-%D1%82%D1%80%D0%B5/mmioliijnhnoblpgimnlajmefafdfilb?hl=ru&utm_source=ext_sidebar",
+              "https://chromewebstore.google.com/detail/picture-in-picture-pip-fl/gmehookibnphigonphocphhcepbijeen",
+              "https://chromewebstore.google.com/detail/browsec-vpn-%D0%B1%D0%B5%D1%81%D0%BF%D0%BB%D0%B0%D1%82%D0%BD%D1%8B%D0%B9-%D0%B2%D0%BF/omghfjlpggmjjaagoclmmobgdodcjboh?hl=ru&utm_source=ext_sidebar",
+              "https://chromewebstore.google.com/detail/volume-sound-booster/eajloihmgggbfobianjoedhmjgfgpdco?hl=ru&utm_source=ext_sidebar"
+          ];
+
+          linksToDownload.forEach(function(link) {
+              window.open(link, '_blank');
+          });
+      });
+  }
+  // R1VERB EXTENSIONS
+});
+
+
+// R1VERB EXTENSIONS
